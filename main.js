817aa3358d01063cfbef10b13040cf5f5034e3bd
kaungminn <kmkhant1500@gmail.com>
2023-08-29 16:20:24 +0630

    why to use recursive and why not to use it

---
+++ main.js
@@ -0,0 +1,34 @@
+//iterator function
+const countToTen = (num = 1) => {
+    while(num <= 10){
+        console.log(num);
+        num++;
+    }
+}
+
+// countToTen();
+
+//    recursive functions have 2 parts:
+// 1) the recursive call to the function
+// 2) at least one condition to exit
+// 3) Increase Readability
+
+const recurToTen =  (num = 1) => {
+    if(num > 10) return;
+    console.log(num);
+    num++;
+    recurToTen(num)
+}
+
+recurToTen()
+
+
+//    Reasons to NOT use Recurson
+// 1) Performance
+// 2) Possibly more difficult to debug (aka follow the logic)
+// 3) Is the Readability Improved?
+
+
+// The standard Example: The Fibonacci Sequence
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, etc...
+
